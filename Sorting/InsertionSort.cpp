fab399fc9545fdbc709b86fc31da5c5e2bde3a7d
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2024-10-10 12:28:55 +0300

    Update InsertionSort.cpp

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -1,1 +1,32 @@
-nxjkkdkdkk
+#include <iostream>
+using namespace std;
+
+void InsertionSort(int A[] , int n){
+    for(int i = 1 , j ; i < n ; ++i){
+        int temp = A[i];
+        for(j = i - 1  ; j >= 0 and temp < A[j] ; --j){
+            A[j + 1] = A[j];
+        }
+        if(i != j + 1)
+         A[j + 1] = temp;
+   
+    }
+}
+void print(int arr[] ,  int n){
+   for(int i = 0 ; i < n ; ++i){
+      cout << arr[i] << " " ;
+   }
+  cout << '\n';
+}
+  
+int main() {
+  int n ;
+  cin >> n;
+  int arr[n];
+  for(int i = 0 ; i < n ; ++i){
+      cin >> arr[i] ;
+  }
+  print(arr , n);
+  InsertionSort(arr , n);
+  print(arr , n);
+}
